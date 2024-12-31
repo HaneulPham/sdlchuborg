62bd72311ab3bf4b00f85ceee29528eeac9dfb18
Haneul Pham <haneulpham@gmail.com>
2024-12-31 20:56:01 +0700

    update test

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Load the header
-    fetch('components/header.html')
+    fetch('../../components/header.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header').innerHTML = data;
       });
   
     // Load the navbar
-    fetch('components/nav.html')
+    fetch('../../components/nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('navbar').innerHTML = data;
       });
   
     // Load the footer
-    fetch('components/footer.html')
+    fetch('../../components/footer.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('footer').innerHTML = data;
